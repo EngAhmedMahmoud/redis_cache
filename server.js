6fa79b0d6a25d5618a339dce261b27a5a615f192
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-02-09 14:45:05 +0400

    Handle http error

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ const SERVER_PORT = process.env.SERVER_PORT;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//handle any http error
+app.use((req, res, next) => {
+    let error = new Error;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    if (error) {
+        error.message = "Not found";
+        error.code = 404;
+    }
+    res.json(error);
+})
 //run server
 app.listen(SERVER_PORT, () => {
     console.log(`Server started ${SERVER_IP}:${SERVER_PORT}`);
