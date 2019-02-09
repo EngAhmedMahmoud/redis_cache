89355e88a9716536d169b9f68f066a3be50e12bc
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-02-09 13:43:48 +0400

    create database connection

---
+++ utils/db.con.js
@@ -0,0 +1,11 @@
+require("dotenv/config");
+const mongoose = require("mongoose");
+const DB_URL = process.env.DB_URL;
+//connection
+mongoose.connect(DB_URL)
+    .then(() => {
+        console.log("database connected successfully");
+    })
+    .catch((err) => {
+        console.log("Error in DB connection:" + err);
+    });
