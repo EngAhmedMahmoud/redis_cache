fb9ad30c4c23546d31da5d838c72e1015cd71144
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-02-09 13:47:12 +0400

    set useNewUrlParser as true for mongo connection

--- utils/db.con.js
+++ utils/db.con.js
@@ -2,7 +2,7 @@ require("dotenv/config");
 const mongoose = require("mongoose");
 const DB_URL = process.env.DB_URL;
 //connection
-mongoose.connect(DB_URL)
+mongoose.connect(DB_URL, { useNewUrlParser: true })
     .then(() => {
         console.log("database connected successfully");
     })
