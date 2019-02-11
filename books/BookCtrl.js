f5fa27ca43bb1bc2d04121b5d0f9e240d20502a7
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-02-11 06:55:32 +0400

    save new book

--- books/BookCtrl.js
+++ books/BookCtrl.js
@@ -2,10 +2,59 @@
 const Book = require("./BookModel");
 
 //all books
-exports.books = async (req, res, next) => {
-    let books = await Book.find();
-    return res.status(200).json({
-        success: 1,
-        books: books
+exports.books = (req, res, next) => {
+    Book.find()
+        .then((books) => {
+            if (books.length != 0) {
+                return res.status(200).json({
+                    success: 1,
+                    books: books
+                });
+            } else {
+                return res.status(200).json({
+                    success: 0
+                });
+            }
+        })
+        .catch((err) => {
+            return res.status(500).json({
+                error: err
+            });
+        })
+
+}
+//save book
+exports.save = (req, res, next) => {
+    let title = req.body.title;
+    let author = req.body.author;
+    let content = req.body.content;
+    let newBook = new Book({
+        title: title,
+        author: author,
+        content: content
     });
+    newBook.save()
+        .then((book) => {
+            res.status(200).json({ success: 1, book });
+        })
+        .catch((err) => {
+            if (err && err.errors) {
+                var errors = err.errors;
+            } else {
+                var errors = err;
+            }
+            res.status(500).json({ success: 0, errors });
+        })
+}
+//delete book
+exports.delete = (req, res, next) => {
+
+}
+//edit book
+exports.edit = (req, res, next) => {
+
+}
+//show book
+exports.show = (req, res, next) => {
+
 }
--- books/BookRoutes.js
+++ books/BookRoutes.js
@@ -3,4 +3,5 @@ const BooksCtrl = require("./BookCtrl");
 const router = require('express').Router();
 
 router.get('/', BooksCtrl.books);
+router.post('/save', BooksCtrl.save);
 module.exports = router;
